d96fe1646c0ff7e45f08db4cc496817bf7bdfc60
yangzom-dolma <166468671+yangzom-dolma@users.noreply.github.com>
2025-07-20 23:35:53 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -47,7 +47,7 @@ let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      apikey: 'd9835cc5',
+      apikey: 'a0ab0f1c',
       i: movie.imdbID
     }
   });
